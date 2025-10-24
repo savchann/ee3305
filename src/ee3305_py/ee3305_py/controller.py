346d8ca11dfec91003c7a02c05f1c6bed6a954a5
savchann <savchanps89@gmail.com>
2025-10-24 11:17:13 +0000

    edited controller

--- src/ee3305_py/ee3305_py/controller.py
+++ src/ee3305_py/ee3305_py/controller.py
@@ -32,14 +32,37 @@ class Controller(Node):
 
         # Handles: Topic Subscribers
         # !TODO: path subscriber
+        self.sub_path_ = self.create_subscription(
+            Path,
+            "path",
+            self.callbackSubPath_,
+            10
+        )
 
         # !TODO: odometry subscriber
+        self.sub_odom_ = self.create_subscription(
+            Odometry,
+            "odom",
+            self.callbackSubOdom_,
+            10,
+        )
+
 
         # Handles: Topic Publishers
         # !TODO: command velocities publisher
+        self.pub_cmd_vel_ = self.create_publisher(
+            TwistStamped,
+            "cmd_vel",
+            10
+        )
 
         # !TODO: lookahead point publisher
-        
+        self.pub_lookahead_ = self.create_publisher(
+            PoseStamped,
+            "lookahead",
+            10
+        )
+
         # Handles: Timers
         self.timer = self.create_timer(1.0 / self.frequency_, self.callbackTimer_)
 
@@ -56,7 +79,7 @@ class Controller(Node):
             return  # do not update the path if no path is returned. This will ensure the copied path contains at least one point when the first non-empty path is received.
 
         # !TODO: copy the array from the path
-        self.path_poses_ = []
+        self.path_poses_ = msg.poses.copy()
 
         self.received_path_ = True
 
@@ -64,9 +87,14 @@ class Controller(Node):
     def callbackSubOdom_(self, msg: Odometry):
         # !TODO: write robot pose to rbt_x_, rbt_y_, rbt_yaw_
         self.rbt_x_ = msg.pose.pose.position.x
+        self.rbt_y_ = msg.pose.pose.position.y
+
 
         q = msg.pose.pose.orientation
-        self.rbt_yaw_ = q.w
+                
+        delta_y = 2 * (q.w * q.z + q.x * q.y)
+        delta_x = 1 - 2 * (q.y ** 2 + q.z ** 2)
+        self.rbt_yaw_ = atan2(delta_y, delta_x)
 
         self.received_odom_ = True
 
